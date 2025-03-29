ddf76c986d9e069b1f545558aafc73096be21aaf
SERGIO ALEJANDRO PINZON BARRIGA <sapinzonb@unadvirtual.edu.co>
2025-03-28 18:32:16 -0600

    aprendiendo de los PropTypes

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,23 +1,31 @@
+import PropTypes from 'prop-types';
 
-const getSaludo = (nombre) => {
-    nombre = 'Alejandro'
-    return `Hola ${nombre}, te estoy mostrando dentro de una expresion de jsx`;
-}
+// const getSaludo = (nombre) => {
+//     nombre = 'Alejandro'
+//     return `Hola ${nombre}, te estoy mostrando dentro de una expresion de jsx`;
+// }
 
 const newMessage = {
     message: 'Hola Mundo',
     title: 'Alejandro Pinzon'
 };
-export const FirstApp = () => {
+export const FirstApp = ( { title, subTitle } ) => {
+    // console.log(props);
+    
     return (
         <>
-            <h1> { getSaludo() } </h1>
+            {/* <h1> { getSaludo() } </h1> */}
             {/* {<code> { JSON.stringify(newMessage) } </code>} */}
-            <p>Soy un Subtitulo</p>
+            <h1> { title }</h1>
+            <p>{ subTitle }</p>
         </>
     )
 }
 
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string
+}
 // { } <= esto se llama expresiones en jsx, aca podemos mostrar elementos de array, numeros, utilizar booleanos pero no se renderizaran
 // pero sobre todo no se puede mostrar objetos y ojo con las funciones asyncronas ya que devuelven una promesa osea objeto y tirara la aplicacion
 // y tambien vemos <> </> <= Esto se le conoce como fragmento  - ermite agrupar estos dos elementos HTML sin crear un elemento contenedor adicional en el DOM, lo que resulta en un marcado más limpio y un mejor rendimiento.
