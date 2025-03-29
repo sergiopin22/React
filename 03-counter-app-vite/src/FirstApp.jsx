9efc3588d903c11c221e75c89448b70a58ecbfb9
SERGIO ALEJANDRO PINZON BARRIGA <sapinzonb@unadvirtual.edu.co>
2025-03-28 23:40:26 -0600

    Use of  Proptypes and defaultProps in react

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -9,22 +9,28 @@ const newMessage = {
     message: 'Hola Mundo',
     title: 'Alejandro Pinzon'
 };
-export const FirstApp = ( { title, subTitle } ) => {
+export const FirstApp = ({ title, subTitle }) => {
     // console.log(props);
-    
+
     return (
         <>
             {/* <h1> { getSaludo() } </h1> */}
             {/* {<code> { JSON.stringify(newMessage) } </code>} */}
-            <h1> { title }</h1>
-            <p>{ subTitle }</p>
+            <h1> {title}</h1>
+            <p>{subTitle}</p>
         </>
     )
 }
 
 FirstApp.propTypes = {
     title: PropTypes.string.isRequired,
-    subTitle: PropTypes.string
+    subTitle: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+    title: 'No hay un titulo',
+    subTitle: 'No hay subtitulo',
+    name: 'Alejandro Pinzon',
 }
 // { } <= esto se llama expresiones en jsx, aca podemos mostrar elementos de array, numeros, utilizar booleanos pero no se renderizaran
 // pero sobre todo no se puede mostrar objetos y ojo con las funciones asyncronas ya que devuelven una promesa osea objeto y tirara la aplicacion
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -7,6 +7,6 @@ import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp title={false}/> 
+        <FirstApp/> 
     </React.StrictMode>
 )
