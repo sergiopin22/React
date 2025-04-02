dfe0c565ca52299123a5a995ca4ab3c3ed70bf6c
SERGIO ALEJANDRO PINZON BARRIGA <sapinzonb@unadvirtual.edu.co>
2025-04-01 23:31:31 -0600

    learning from Expect and Tobe in Jest

--- 03-counter-app-vite/test/demo.test.js
+++ 03-counter-app-vite/test/demo.test.js
@@ -1,5 +1,15 @@
-test('Esta Prueba no debe de fallar', () => {
-    if (0 === 0){
-        throw new Error('no puede dividir entre cero');
-    }
-})
+describe('Pruebas en <DemoComponent />', () => { 
+
+    test('Esta Prueba no debe de fallar', () => {
+    
+        //1. Inicializacion
+        const message1 = 'Hola Mundo';
+        //2/ Estimulo 
+        const message2 = message1.trim();
+        //3. Observar el comportamiento... esperado
+        expect( message1 ).toBe( message2 );
+    
+    })
+    
+ })
+
