763af36a51953dc4cc91374def6cb1307d2b5cb3
SERGIO ALEJANDRO PINZON BARRIGA <sapinzonb@unadvirtual.edu.co>
2025-04-09 00:09:23 -0600

    evaluating Catch in async - await in jest

--- 03-counter-app-vite/src/base-pruebas/base-pruebas/11-async-await.js
+++ 03-counter-app-vite/src/base-pruebas/base-pruebas/11-async-await.js
@@ -11,7 +11,7 @@ export const getImagen = async() => {
         return url;
     } catch (error) {
         // manejo del error
-        console.error(error)
+        // console.error(error)
         return 'No se encontro la imagen';
     }
     
--- 03-counter-app-vite/test/base-pruebas/11-async-await.test.js
+++ 03-counter-app-vite/test/base-pruebas/11-async-await.test.js
@@ -2,12 +2,13 @@ import { getImagen } from "../../src/base-pruebas/base-pruebas/11-async-await"
 
 describe('Preubas en 11-async-await.js', () => { 
 
-    test('getImagen debe de retornar un URL de la imagen', async() => { 
+    test('getImagen debe de retornar un error si no tenemos API KEY ', async() => { 
 
         const url  = await getImagen();
-        console.log(url);
 
-        expect( typeof url ).toBe('string');
+        // expect( typeof url ).toBe('string');
+
+        expect( resp ).toBe('No se pudo encontrar la imagen');
 
      })
 
