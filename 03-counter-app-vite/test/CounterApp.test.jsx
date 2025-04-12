24d804a1a099b40c7024cdcda91869d62def76bc
SERGIO ALEJANDRO PINZON BARRIGA <sapinzonb@unadvirtual.edu.co>
2025-04-11 18:03:59 -0600

    simulating click events in the CounterApp

--- 03-counter-app-vite/test/CounterApp.test.jsx
+++ 03-counter-app-vite/test/CounterApp.test.jsx
@@ -1,27 +1,53 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
-describe('Pruebas en <CounterApp/>', () => { 
+describe('Pruebas en <CounterApp/>', () => {
 
     const value = 100;
-    test('debe de hacer match con el snapsshot', () => { 
+    test('debe de hacer match con el snapsshot', () => {
 
-        const { container } = render( 
-            <CounterApp value={value}/>
-         );
+        const { container } = render(
+            <CounterApp value={value} />
+        );
 
-         expect( container ).toMatchSnapshot();
+        expect(container).toMatchSnapshot();
 
-     });
+    });
 
-     test('debe de mostar el valor inicial de 100', () => { 
+    test('debe de mostar el valor inicial de 100', () => {
 
-        render( <CounterApp value={value}/> );
-        expect( screen.getAllByText(value)).toBeTruthy();
+        render(<CounterApp value={value} />);
+        expect(screen.getAllByText(value)).toBeTruthy();
         //otra forma seria
-        expect( screen.getByRole('heading',{ level: 2 }).innerHTML ).toContain('100');
+        expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain('100');
 
-      })
+    });
 
+    test('debe de incrementear con el boton +1', () => {
 
- })
+        render(<CounterApp value={value} />);
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText('101')).toBeTruthy();
+    });
+
+    test('debe de decrementar con el boton -1', () => {
+
+        render(<CounterApp value={value} />);
+        fireEvent.click(screen.getByText('-1'));
+        expect(screen.getByText('99')).toBeTruthy();
+    });
+
+    test('debe de funcionar el boton de reset', () => { 
+
+        render(<CounterApp value={value} />);
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        // fireEvent.click(screen.getByText('Reset'));
+
+        fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }));
+
+        expect( screen.getByText(value) ).toBeTruthy();
+
+     })
+
+})
